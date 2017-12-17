1536aee26bca3098c17b7f96e37c779dc98315e8
thongpitak <thongpitak.sowanna@gmail.com>
2017-12-17 15:02:33 +0700

    offline Detail

---
+++ app/src/main/java/com/example/admin/sticktree/offline_detail.java
@@ -0,0 +1,13 @@
+package com.example.admin.sticktree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class offline_detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline_detail);
+    }
+}
