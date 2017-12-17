72e120b2f8d64f6b52e686758e8729ec6421a31d
thongpitak <thongpitak.sowanna@gmail.com>
2017-12-17 14:28:10 +0700

    edit Login layout And add online update

---
+++ app/src/main/java/com/example/admin/sticktree/online_updateTree.java
@@ -0,0 +1,13 @@
+package com.example.admin.sticktree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class online_updateTree extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_online_update_tree);
+    }
+}
